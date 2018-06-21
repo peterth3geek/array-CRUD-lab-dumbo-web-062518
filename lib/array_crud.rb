4df16f4b7da6c97f3c6b6db3e0af1467a9c22eee
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-21 17:15:15 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.index()
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.index(array.length - 1)
 end
