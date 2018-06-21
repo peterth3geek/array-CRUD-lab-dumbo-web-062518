9dac68c77fffd1ab8d9dbaaf8a1f279964ae6ae2
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-21 17:17:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index(index_number)
+  array(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.index()
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array.index(array.length - 1)
+  array.last
 end
